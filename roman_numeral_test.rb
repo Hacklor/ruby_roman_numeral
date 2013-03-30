f903907527122e6fbb79de5a49207c233b3a0d1b
Nicole Bekkers <hackybekkers@hacklor.com>
2013-03-30 21:31:59 +0100

    Refactored out RomanNumeral.new

--- roman_numeral_test.rb
+++ roman_numeral_test.rb
@@ -3,31 +3,35 @@ require_relative "roman_numeral"
 
 class RomanNumeralTest < Test::Unit::TestCase
 
+  def setup
+    @numerals = RomanNumeral.new
+  end
+
   def test_zero
-    assert_equal "", RomanNumeral.new.to_roman(0)
+    assert_equal "", @numerals.to_roman(0)
   end
 
   def test_single_numeral
-    assert_equal "I", RomanNumeral.new.to_roman(1)
-    assert_equal "V", RomanNumeral.new.to_roman(5)
-    assert_equal "X", RomanNumeral.new.to_roman(10)
-    assert_equal "L", RomanNumeral.new.to_roman(50)
-    assert_equal "C", RomanNumeral.new.to_roman(100)
-    assert_equal "D", RomanNumeral.new.to_roman(500)
-    assert_equal "M", RomanNumeral.new.to_roman(1000)
+    assert_equal "I", @numerals.to_roman(1)
+    assert_equal "V", @numerals.to_roman(5)
+    assert_equal "X", @numerals.to_roman(10)
+    assert_equal "L", @numerals.to_roman(50)
+    assert_equal "C", @numerals.to_roman(100)
+    assert_equal "D", @numerals.to_roman(500)
+    assert_equal "M", @numerals.to_roman(1000)
   end
 
   def test_additions
-    assert_equal "II", RomanNumeral.new.to_roman(2)
-    assert_equal "VI", RomanNumeral.new.to_roman(6)
+    assert_equal "II", @numerals.to_roman(2)
+    assert_equal "VI", @numerals.to_roman(6)
   end
 
   def test_subtractions
-    assert_equal "IV", RomanNumeral.new.to_roman(4)
-    assert_equal "IX", RomanNumeral.new.to_roman(9)
-    assert_equal "XL", RomanNumeral.new.to_roman(40)
-    assert_equal "XC", RomanNumeral.new.to_roman(90)
-    assert_equal "CD", RomanNumeral.new.to_roman(400)
-    assert_equal "CM", RomanNumeral.new.to_roman(900)
+    assert_equal "IV", @numerals.to_roman(4)
+    assert_equal "IX", @numerals.to_roman(9)
+    assert_equal "XL", @numerals.to_roman(40)
+    assert_equal "XC", @numerals.to_roman(90)
+    assert_equal "CD", @numerals.to_roman(400)
+    assert_equal "CM", @numerals.to_roman(900)
   end
 end
